70d561b82d0729701d441cab5cb51ef937e78e9c
idamakela <idamakela98@gmail.com>
2023-01-16 14:26:32 +0100

    castspell function done, need to finish the onclick for it

--- generator/js/app.js
+++ generator/js/app.js
@@ -1,88 +1,60 @@
 $(function() {
     const API_ADDRESS = "https://www.dnd5eapi.co";
 
+    //onclick function, missing a reload function. Data is just added on every click
+    $(".start").on("click", castSpell);
 
-    fetch(API_ADDRESS + "/api/spells/")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-
-        let randomSpell = generateRandomSpell(data.results);
-        console.log(randomSpell)
-
-        fetch(API_ADDRESS + randomSpell.url)
+    function castSpell() {
+        fetch(API_ADDRESS + "/api/spells/")
         .then(response => response.json())
         .then(data => {
-            console.log(data)
-
-            let spellsClass = [];
-            getClasses();
-            function getClasses(){
+            let randomSpell = generateRandomSpell(data.results);
+    
+            fetch(API_ADDRESS + randomSpell.url)
+            .then(response => response.json())
+            .then(data => {
+                let spellsClass = [];
+                let spellsComponents = [];
+                let spellsDesc = [];
+    
                 for(i = 0; i <= data.classes.length - 1; i++) {
                     spellsClass.push(data.classes[i].name) 
                 }
-            }
-
-            let spellsComponents = [];
-            getComponents();
-            function getComponents() {
+    
                 for(i = 0; i <= data.components.length - 1; i++) {
                     spellsComponents.push(data.components[i]) 
                 }
-            }
-
-            let spellsDesc = [];
-            getDesc();
-            function getDesc() {
+            
                 for(i = 0; i <= data.desc.length - 1; i++) {
                     spellsDesc.push(data.desc[i]) 
                 }
-            }
-            
-
-            
-            //other code
-            $(".name").text(data.name);
-            $(".first").append("<p>Level " + data.level + " " +  data.school.name  + " | " + spellsClass.join(", ") + "</p>");
-
-
-
-            $(".second").append("<p><b>Range: </b>" + data.range + "</p>");
-            $(".second").append("<p><b>Casting time: </b>" + data.casting_time + "</p>");
-
-            if(data.ritual == true) {
-                $(".second").append("<p><b>Ritual: </b>yes</p>");
-            } else {
-                $(".second").append("<p><b>Ritual: </b>no</p>");
-            }
-
-            if(data.concentration == true) {
-                $(".second").append("<p><b>Concentration: </b>yes</p>");
-            } else {
-                $(".second").append("<p><b>Concentration: </b>no</p>");
-            }
-
-            $(".second").append("<p><b>Duration: </b>" + data.duration + "</p>");
-            $(".second").append("<p><b>Components: </b>" + spellsComponents.join(", ") + "</p>");
-            $(".second").append("<p><b>Materials: </b>" + data.material + "</p>");
-
-            $(".third").append($("<p>").text(spellsDesc.join(" ")));
-            
-        })
-
-
-    })
+                
+                $(".name").text(data.name);
+    
+                if(data.ritual == true && data.concentration == true) {
+                    $(".zero").append("<p><i>Ritual spell</i> | <i>Concentration spell</i></p>")
+                } else if(data.ritual == true) {
+                    $(".zero").append("<p><i>Ritual spell</i></p>")
+                } else if(data.concentration == true) {
+                    $(".zero").append("<p><i>Concentration spell</i></p>")
+                }
+    
+                $(".first").append("<p>Level " + data.level + " " +  data.school.name  + " | " + spellsClass.join(", ") + "</p>");
+                $(".second").append("<p><b>Range: </b>" + data.range + "</p>");
+                $(".second").append("<p><b>Casting time: </b>" + data.casting_time + "</p>");
+                $(".second").append("<p><b>Duration: </b>" + data.duration + "</p>");
+                $(".second").append("<p><b>Components: </b>" + spellsComponents.join(", ") + "</p>");
+    
+                if(data.material != undefined) {
+                    $(".second").append("<p><b>Materials: </b>" + data.material + "</p>");
+                } 
+    
+                $(".third").append($("<p>" + spellsDesc.join("<br><br>") + "</p>"));
+            });
+        });
+    }
 
     function generateRandomSpell(targetArray) {
         return targetArray[Math.floor(Math.random() * targetArray.length)];
     }
-
-
-    /*
-        All spells: data.results
-        Specific spell: data.results[x]
-            generate random number for x to generate random specific spell
-        Fetch the random spells url: API_ADDRESS + data.results[i].url
-        Append spells results in html
-    */
-})
+});
