d9a191988e94f5a563f371168b99738d18c334bc
Ida Mäkelä <idamakela98@gmail.com>
2023-01-19 11:21:53 +0100

    fixed code for spellsearch before the error test

--- js/app.js
+++ js/app.js
@@ -1,53 +1,6 @@
 $(function() {
     const API_ADDRESS = "https://www.dnd5eapi.co";
     const API_SPELL = "https://api.open5e.com/spells";
-
-    //LOGICAL TEST VARIABLES 
-    let result = false;
-    //
-
-    $(".spell-result").hide();
-
-    $(".randomize").click(function() {
-
-        //Make the select go to the first value
-        //$("#class").find("option:none").attr("selected", "selected");
-        //$("#level").find("option.none").attr("selected", "selected");
-
-        $(".box").empty();
-        $(".spell-result").show()
-        castRandomSpell();
-    });
-
-    $(".search").click(function() {
-        $(".box").empty();
-        $(".spell-result").show();
-        searchSpell();
-    });
-
-
-
-
-    //LOGICAL TEST 2
-    let classInput = "sorcerer";
-    let levelInput = 2;
-    
-    //while loop has relation to if else in the function
-        //the result variable needs to be set in if else in function 
-    /*
-        q = 0
-    while(q <= 11) {
-        if(classInput == undefined || levelInput == undefined) {
-            $(".name").text("Please select a class and level to proceed")
-            q++;
-        } else {
-            testPageResult(API_SPELL);
-            console.log("false");
-            q++;
-        }
-    }
-    console.log("while loop is broken")
-    */
     const API_PAGES = [
         "",
         "/?page=2",
@@ -68,47 +21,21 @@ $(function() {
         "/?page=17"
     ];
 
-   testPageResult(API_SPELL, API_PAGES);
-   
-   
-   function testPageResult(targetApi, targetApiPage) {
-        //Can I have this outside the function??
-        //let classInput = $("#class").val();
-        //let levelInput = $("#level").val();
+    $(".spell-result").hide();
 
+    $(".randomize").click(function() {
+        $(".box").empty();
+        $(".spell-result").show()
+        castRandomSpell();
+    });
 
-        let apiPage = randomGenerator(targetApiPage);
-        let apiAddress = targetApi + apiPage;
+    $(".search").click(function() {
+        $(".box").empty();
+        $(".spell-result").show();
+        searchSpell(API_SPELL, API_PAGES); //ERROR: when page does not have any result from input, h2 append does not disappear either 
+    });
 
-
-        //prob something weird with fetch 
-        fetch(apiAddress)
-        .then((response) => response.json())
-        .then((data) => {
-            //issue with fetch according to chrome debugger
-            console.log(data);
-            
-            
-
-            for(let x = 0; x <= data.results.length -1; x++) {
-
-                if(data.results[x].dnd_class.toLowerCase().includes(classInput) && data.results[x].level_int == levelInput) {
-                    result = true;
-                    console.log(result);
-                } else {
-                    result = false;
-                    console.log(result);
-                }
-            
-            }
-        })
-    }
-
-
-
-
-
-    function searchSpell() {
+    function searchSpell(targetApi, targetApiPage) {
         let classInput = $("#class").val();
         let levelInput = $("#level").val();
         
@@ -116,109 +43,51 @@ $(function() {
             $(".name").text("Please select a class and level to proceed")
         }
 
-        let apiPages = [
-            " ",
-            "/?page=2",
-            "/?page=3",
-            "/?page=4",
-            "/?page=5",
-            "/?page=6",
-            "/?page=7",
-            "/?page=8",
-            "/?page=9",
-            "/?page=10",
-            "/?page=11",
-            "/?page=12",
-            "/?page=13",
-            "/?page=14",
-            "/?page=15",
-            "/?page=16",
-            "/?page=17"
-        ];
         let apiResults = [];
-
-        let apiPage = randomGenerator(apiPages);
-        let apiAddress = API_SPELL + apiPage;
+        let apiPage = randomGenerator(targetApiPage);
+        let apiAddress = targetApi + apiPage;
 
         fetch(apiAddress)
-        .then((response) => response.json())
-        .then((data) => {
-            
-            //LOGICAL TEST
-            let testPage = null;
-
-            for(let x = 0; x <= data.results.length -1; x++) {
-                if(data.results[x].dnd_class.toLowerCase().includes(classInput) && data.results[x].level_int == levelInput) {
-                    testPage = true;
-                    console.log(testPage);
-                    break;
-                } else {
-                    testPage = false;
-                    console.log(testPage);
-                }
-            }
-
-            //RESULT OF ABOVE TEST
-            if(testPage == true) {
-                console.log("true is true");
-                appendSpell();
+        .then((response) => {
+            if(!response.ok) {
+                throw new Error(response.status);
             } else {
-                console.log("false is false");
-                
-                apiPage = randomGenerator(apiPages);
-                console.log(apiPage);
-                
-                appendSpell();
-                /*
-                fetch(API_SPELL + apiPage)
-                .then((response) => response.json())
-                .then((data) => {
-                    appendSpell()
-                })*/
-
+                return response.json();
             }
-            //LOGICAL FUNCTION ABOVE
-
-            
-            
-            //CODE
-            function appendSpell() {
-
-                //generate random spell from input 
-                let apiResults = [];
-
-                for(let i = 0; i <= data.results.length - 1; i++) {
-                    if(data.results[i].dnd_class.toLowerCase().includes(classInput) && data.results[i].level_int == levelInput) {
-                        apiResults.push(data.results[i])
-                    } 
-                }
-    
-                let spellResult = randomGenerator(apiResults);
-                console.log(spellResult);
-    
-                //append result
-                $(".name").text(spellResult.name);
-                $(".first").append("<p>" + spellResult.level + " " +  spellResult.school  + " | " + spellResult.dnd_class + "</p>");
-                
-                if(spellResult.ritual.includes("yes") && spellResult.concentratrion.includes("yes")) {
-                    $(".second").append("<p><b>Ritual spell</b> | <b>Concentration spell</b></p>");
-                } else if(spellResult.ritual.includes("yes")) {
-                    $(".second").append("<p><b>Ritual spell</b></p>");
-                } else if(spellResult.concentration.includes("yes")) {
-                    $(".second").append("<p><b>Concentration spell</b></p>");
-                }
-    
-                $(".second").append("<p><b>Range: </b>" + spellResult.range + "</p>");
-                $(".second").append("<p><b>Casting time: </b>" + spellResult.casting_time + "</p>");
-                $(".second").append("<p><b>Duration: </b>" + spellResult.duration + "</p>");
-                $(".second").append("<p><b>Components: </b>" + spellResult.components + "</p>");
-    
-                if(spellResult.material != "") {
-                    $(".second").append("<p><b>Materials: </b>" + spellResult.material + "</p>");
+        })
+        .then((data) => {
+            for(let i = 0; i <= data.results.length - 1; i++) {
+                if(data.results[i].dnd_class.toLowerCase().includes(classInput) && data.results[i].level_int == levelInput) {
+                    apiResults.push(data.results[i])
                 } 
-    
-                $(".third").append($("<p>" + spellResult.desc + "</p>"));
             }
+
+            let spellResult = randomGenerator(apiResults);
+
+            $(".name").text(spellResult.name);
+            $(".first").append("<p>" + spellResult.level + " " +  spellResult.school  + " | " + spellResult.dnd_class + "</p>");
+            
+            if(spellResult.ritual.includes("yes") && spellResult.concentratrion.includes("yes")) {
+                $(".second").append("<p><b>Ritual spell</b> | <b>Concentration spell</b></p>");
+            } else if(spellResult.ritual.includes("yes")) {
+                $(".second").append("<p><b>Ritual spell</b></p>");
+            } else if(spellResult.concentration.includes("yes")) {
+                $(".second").append("<p><b>Concentration spell</b></p>");
+            }
+
+            $(".second").append("<p><b>Range: </b>" + spellResult.range + "</p>");
+            $(".second").append("<p><b>Casting time: </b>" + spellResult.casting_time + "</p>");
+            $(".second").append("<p><b>Duration: </b>" + spellResult.duration + "</p>");
+            $(".second").append("<p><b>Components: </b>" + spellResult.components + "</p>");
+
+            if(spellResult.material != "") {
+                $(".second").append("<p><b>Materials: </b>" + spellResult.material + "</p>");
+            } 
+
+            $(".third").append($("<p>" + spellResult.desc + "</p>"));
+        })
+        .catch((error) => {
+            $("main").append($("<h2>Something went wrong: " + error + "</h2>"));
         })
     }
 
