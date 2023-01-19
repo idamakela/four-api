9330ce653decfb2dc0940bafd702c049a6330c7f
Ida Mäkelä <idamakela98@gmail.com>
2023-01-19 13:20:26 +0100

    found errors, se comments

--- js/app.js
+++ js/app.js
@@ -1,53 +1,7 @@
+//THIS IS ONLY FOR WORKING ON TESTING PAGE WITH INPUT LOOP
+
 $(function() {
-    const API_ADDRESS = "https://www.dnd5eapi.co";
     const API_SPELL = "https://api.open5e.com/spells";
-
-    //LOGICAL TEST VARIABLES 
-    let result = false;
-    //
-
-    $(".spell-result").hide();
-
-    $(".randomize").click(function() {
-
-        //Make the select go to the first value
-        //$("#class").find("option:none").attr("selected", "selected");
-        //$("#level").find("option.none").attr("selected", "selected");
-
-        $(".box").empty();
-        $(".spell-result").show()
-        castRandomSpell();
-    });
-
-    $(".search").click(function() {
-        $(".box").empty();
-        $(".spell-result").show();
-        searchSpell();
-    });
-
-
-
-
-    //LOGICAL TEST 2
-    let classInput = "sorcerer";
-    let levelInput = 2;
-    
-    //while loop has relation to if else in the function
-        //the result variable needs to be set in if else in function 
-    /*
-        q = 0
-    while(q <= 11) {
-        if(classInput == undefined || levelInput == undefined) {
-            $(".name").text("Please select a class and level to proceed")
-            q++;
-        } else {
-            testPageResult(API_SPELL);
-            console.log("false");
-            q++;
-        }
-    }
-    console.log("while loop is broken")
-    */
     const API_PAGES = [
         "",
         "/?page=2",
@@ -68,39 +22,59 @@ $(function() {
         "/?page=17"
     ];
 
-   testPageResult(API_SPELL, API_PAGES);
-   
-   
-   function testPageResult(targetApi, targetApiPage) {
-        //Can I have this outside the function??
-        //let classInput = $("#class").val();
-        //let levelInput = $("#level").val();
 
 
+    //LOGICAL TEST VARIABLES 
+    let testVar = 0;
+    let classInput = "sorcerer";
+    let levelInput = 2;
+    //
+
+    $(".spell-result").hide();
+
+    $(".search").click(function() {
+        $(".box").empty();
+        $(".spell-result").show();
+
+    });
+
+    function test() {
+        //these need to be incide the function 
+        let classInput = $("#class").val();
+        let levelInput = $("#level").val();
+        console.log(classInput)
+        console.log(levelInput)
+    }
+
+    //LOGICAL TEST
+    testPageResult(API_SPELL, API_PAGES);
+    console.log(testVar);
+
+    function testPageResult(targetApi, targetApiPage) {
         let apiPage = randomGenerator(targetApiPage);
         let apiAddress = targetApi + apiPage;
+        console.log(classInput + " " + levelInput)
 
-
-        //prob something weird with fetch 
+        //ERROR: could be fetch, seem like there is a ajax async set to false method 
         fetch(apiAddress)
         .then((response) => response.json())
         .then((data) => {
-            //issue with fetch according to chrome debugger
             console.log(data);
-            
-            
+            console.log(data.results)
 
-            for(let x = 0; x <= data.results.length -1; x++) {
-
-                if(data.results[x].dnd_class.toLowerCase().includes(classInput) && data.results[x].level_int == levelInput) {
-                    result = true;
-                    console.log(result);
+            //ERROR: boolean expression
+            for(i = 0; i <= data.results.length - 1; i++) {
+                if(data.results[i].dnd_class.toLowerCase().includes(classInput) && data.results[i].level_int == levelInput) {
+                    testVar = 1;
+                    console.log(testVar)
+                    break;
                 } else {
-                    result = false;
-                    console.log(result);
+                    console.log(testVar)
+
+                    continue;
                 }
-            
             }
+            
         })
     }
 
@@ -108,6 +82,13 @@ $(function() {
 
 
 
+
+
+
+
+
+
+    /*
     function searchSpell() {
         let classInput = $("#class").val();
         let levelInput = $("#level").val();
@@ -169,19 +150,18 @@ $(function() {
                 console.log(apiPage);
                 
                 appendSpell();
-                /*
                 fetch(API_SPELL + apiPage)
                 .then((response) => response.json())
                 .then((data) => {
                     appendSpell()
-                })*/
+                })
 
             }
             //LOGICAL FUNCTION ABOVE
 
             
             
-            //CODE
+            //CODE: feel like i could break this appart in functions 
             function appendSpell() {
 
                 //generate random spell from input 
@@ -220,76 +200,8 @@ $(function() {
                 $(".third").append($("<p>" + spellResult.desc + "</p>"));
             }
         })
-    }
+    } */
 
-    //FINISHED FUNCTION FOR RANDOMIZER
-    function castRandomSpell() {
-        fetch(API_ADDRESS + "/api/spells/")
-        .then((response) => {
-            if(!response.ok) {
-                throw new Error(response.status);
-            } else {
-                return response.json();
-            }
-        })
-        .then((data) => {
-            let randomSpell = randomGenerator(data.results);
-    
-            fetch(API_ADDRESS + randomSpell.url)
-            .then((response) => {
-                if(!response.ok) {
-                    throw new Error(response.status);
-                } else {
-                    return response.json();
-                }
-            })
-            .then((data) => {
-                let spellsClass = [];
-                let spellsComponents = [];
-                let spellsDesc = [];
-    
-                for(let x = 0; x <= data.classes.length - 1; x++) {
-                    spellsClass.push(data.classes[x].name);
-                }
-    
-                for(let y = 0; y <= data.components.length - 1; y++) {
-                    spellsComponents.push(data.components[y]);
-                }
-            
-                for(let z = 0; z <= data.desc.length - 1; z++) {
-                    spellsDesc.push(data.desc[z]);
-                }
-                
-                $(".name").text(data.name);
-                $(".first").append("<p>Level " + data.level + " " +  data.school.name  + " | " + spellsClass.join(", ") + "</p>");
-    
-                if(data.ritual == true && data.concentration == true) {
-                    $(".second").append("<p><b>Ritual spell</b> | <b>Concentration spell</b></p>");
-                } else if(data.ritual == true) {
-                    $(".second").append("<p><b>Ritual spell</b></p>");
-                } else if(data.concentration == true) {
-                    $(".second").append("<p><b>Concentration spell</b></p>");
-                }
-    
-                $(".second").append("<p><b>Range: </b>" + data.range + "</p>");
-                $(".second").append("<p><b>Casting time: </b>" + data.casting_time + "</p>");
-                $(".second").append("<p><b>Duration: </b>" + data.duration + "</p>");
-                $(".second").append("<p><b>Components: </b>" + spellsComponents.join(", ") + "</p>");
-    
-                if(data.material != undefined) {
-                    $(".second").append("<p><b>Materials: </b>" + data.material + "</p>");
-                } 
-    
-                $(".third").append($("<p>" + spellsDesc.join("<br><br>") + "</p>"));
-            })
-            .catch((error) => {
-                $("main").append($("<h2>Something went wrong: " + error + "</h2>"));
-            })
-        })
-        .catch((error) => {
-            $("main").append($("<h2>Something went wrong: " + error + "</h2>"));
-        })
-    }
 
     function randomGenerator(targetArray) {
         return targetArray[Math.floor(Math.random() * targetArray.length)];
